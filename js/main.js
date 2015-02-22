a08653e1d0fafc84424c1bbc359d58fe297bc768
benjaminran <benjaminran2@gmail.com>
2015-02-21 22:14:19 -0800

    added slider bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+function setupSlider() {
+	// First of all attach a slider to an element. If you want to customize the range of values, you do it in the initialization.
+	$('.user-slider').slider({min: 0.0, max: 1.0, step: 0.001});
+
+	// Then you can give it pips and labels!  
+	$('.user-slider').slider('pips', {  
+	    first: '0.0',  
+	    last: '1.0',  
+	    rest: 'flase',  
+	    prefix: "",  
+	    suffix: ""  
+	});
+
+	// And finally can add floaty numbers (if desired)  
+	$('.user-slider').slider('float', {  
+	    handle: true,  
+	    pips: true,  
+	    prefix: "",  
+	    suffix: ""  
+	});
+}
+
 $(window).load(function() {
 	$.plot($("#placeholder"), [ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
+	setupSlider();
 });
